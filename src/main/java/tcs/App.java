19657599d6b12e054b5b81d3dc5017b3794bda8a
root <root@ip-172-31-24-36.ap-south-1.compute.internal>
2019-03-17 11:47:54 +0000

    updated

--- src/main/java/tcs/App.java
+++ src/main/java/tcs/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello gudevng World!" );
+        System.out.println( "Hello good World!" );
     }
 }
